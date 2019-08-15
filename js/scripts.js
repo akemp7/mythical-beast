ce079665e4538c2f01a911e8383830c554375f92
Anita Kemp and Jason Huels <jason@>
2019-08-15 10:54:48 -0700

    add questions and javascipt code

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("#submit").click(function(event) {
+    var terrain = $("#terrain").val();
+    var plumage = $("#plumage").val();
+    
+
+
+
+    console.log(plumage);
+
+    event.preventDefault();
+  });
+});
