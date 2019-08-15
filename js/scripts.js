ca54ebb0a2ab72ab835c1f565c0159aeda63e0a7
Anita Kemp and Jason Huels <jason@>
2019-08-15 11:30:49 -0700

    add branching code to website

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
 $(function() {
   $("#submit").click(function(event) {
-    var terrain = $("#terrain").val();
-    var plumage = $("#plumage").val();
-    
+    var terrain = $("#terrain").val().toLowerCase();
+    var plumage = $("#plumage").val().toLowerCase();;
 
+    $(".results").hide();
 
-
-    console.log(plumage);
-
+    if(terrain === "land") {
+      if(plumage === "feathers") {
+        $("#elwetritsch").show();
+      } else if(plumage === "scales") {
+        $("#zhulong").show();
+      } else if(plumage === "hair") {
+        $("#centaur").show();
+      }
+    } else if(terrain === "sea") {
+      if(plumage === "feathers") {
+        $("#siren").show();
+      } else if(plumage === "scales") {
+        $("#loch-ness").show();
+      } else if(plumage === "hair") {
+        $("#mermaid").show();
+      }
+    } else if(terrain === "air") {
+      if(plumage === "feathers") {
+        $("#griffon").show();
+      } else if(plumage === "scales") {
+        $("#dragon").show();
+      } else if(plumage === "hair") {
+        $("#harpy").show();
+      }
+    }
     event.preventDefault();
   });
 });
